c3d432771037a01db53a231aea4565406f4ce9c3
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-16 07:47:18 +0200

    Update task-02.js
    Завдання 2 Cкрипт, який для кожного елемента масиву ingredients:
    
    Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
    Додасть назву інгредієнта як його текстовий вміст.
    Додасть елементу клас item.
    Після чого, вставить усі <li> за одну операцію у список ul#ingredients.

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,6 @@
+// Отримання порожнього елемента <ul>
+const ul = document.getElementById('ingredients');
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +9,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Перебираємо масив інгредієнтів і створюємо елемент <li> для кожного
+const liElements = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+//використовуємо оператор spread (...), щоб одночасно вставити всі елементи <li> в елемент <ul> за допомогою методу append().
+ul.append(...liElements);
